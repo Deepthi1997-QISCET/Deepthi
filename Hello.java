716f2383e115cd752932d394009b7131e68aa7dd
Deepthi1997-QISCET <41726078+Deepthi1997-QISCET@users.noreply.github.com>
2018-07-27 10:06:13 +0530

    Hello.java

---
+++ Hello.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Hello
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	int i,n=2;
+	for(i=0;i<n;i++)
+	{
+		System.out.println("Hello World\n");
+	}
+	}
+}

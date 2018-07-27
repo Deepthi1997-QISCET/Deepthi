7e8b830a1ab48a9e62cf1b2a01d9b8b27d4ee316
Deepthi1997-QISCET <41726078+Deepthi1997-QISCET@users.noreply.github.com>
2018-07-27 12:24:03 +0530

    Prime.java

---
+++ Prime.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Prime
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	int i=11;
+	if(i%1==0&&i%i==0)
+	{
+	  System.out.println("yes");
+	}
+	else
+	{
+		System.out.println("no");
+	}
+	}
+}

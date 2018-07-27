04399a0c0e3733d0d280bc65c5c0366c40866de2
Deepthi1997-QISCET <41726078+Deepthi1997-QISCET@users.noreply.github.com>
2018-07-27 10:22:57 +0530

    Naturalnum.java

---
+++ Naturalnum.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Naturalnum
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int n=5,i,sum=0;
+		for(i=1;i<=n;i++)
+		{
+		sum=sum+i;
+		}
+		System.out.println("sum=" +sum);
+	}
+}

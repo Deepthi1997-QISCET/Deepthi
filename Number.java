d827c09a82b911bbcfbae4ceaeab11127549b91b
Deepthi1997-QISCET <41726078+Deepthi1997-QISCET@users.noreply.github.com>
2018-07-27 06:39:09 +0530

    Number.java

---
+++ Number.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Number
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int num=7;
+		if(num%2==0)
+		{
+			System.out.println("num is even");
+		}
+		else
+		{
+			System.out.println("num is odd");
+		}
+	}
+}

8d63c23b14288716aa5d819d3baaaba265b64b2c
Deepthi1997-QISCET <41726078+Deepthi1997-QISCET@users.noreply.github.com>
2018-07-27 13:04:47 +0530

    Even.java

---
+++ Even.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Even
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		for(int i=1; i < 10; i++)
+		{
+			if( i % 2 == 0)
+			{
+		             System.out.print(i + " ");
+			}
+		}
+  }
+		
+}

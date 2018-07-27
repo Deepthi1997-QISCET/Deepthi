62a8f22f82235e34cb4b690aaf5ecaeb49036af9
Deepthi1997-QISCET <41726078+Deepthi1997-QISCET@users.noreply.github.com>
2018-07-27 07:04:29 +0530

    Character.java

---
+++ Character.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Character
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	            char ch='a';
+		if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')
+		{
+			System.out.println(ch + "is vowel");
+		}
+		else
+		{
+			System.out.println(ch + "is consonant");
+		}
+	}
+}

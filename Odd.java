cd5dbacdb72b749a60397a9a76c0e7c56153ae90
Deepthi1997-QISCET <41726078+Deepthi1997-QISCET@users.noreply.github.com>
2018-07-27 12:53:39 +0530

    Odd.java

---
+++ Odd.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Odd
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		for(int i=2; i < 6; i++)
+		{
+			if( i % 2 != 0)
+			{
+		             System.out.print(i + " ");
+			}
+		 }
+	}
+}

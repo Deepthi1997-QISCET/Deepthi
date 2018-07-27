7314a2ef5d37066b9221c5de3736f771c62447c3
Deepthi1997-QISCET <41726078+Deepthi1997-QISCET@users.noreply.github.com>
2018-07-27 13:12:45 +0530

    Ideone.java

--- Ideone.java
+++ Ideone.java
@@ -5,18 +5,18 @@ class Ideone
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-              int a=5;
+              int a=3;
               if(a>0)
               {
-               System.out.println("a is positive");
+               System.out.println("positive");
               }
              else if(a<0)
               {
-              System.out.println("a is negative");
+              System.out.println("negative");
               }
               else
               {
-              System.out.println("a is zero");	
+              System.out.println("zero");	
               }
 	}
 }

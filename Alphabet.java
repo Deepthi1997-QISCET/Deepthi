efc6c61eca157087a1eff4892e42eb6d01bbf3bd
Deepthi1997-QISCET <41726078+Deepthi1997-QISCET@users.noreply.github.com>
2018-07-27 07:15:44 +0530

    Alphabet.java

---
+++ Alphabet.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Alphabet
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	            char c='A';
+		if((c>='a'&&c<='z')||(c>='A'&&c<='Z'))
+		{
+			System.out.println(c + "is a alphabet");
+		}
+		else
+		{
+			System.out.println(c + "is not an alphabet");
+		}
+	}
+}

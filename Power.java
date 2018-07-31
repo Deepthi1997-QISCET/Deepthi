876338e72dbf8ead47757116f4fd5b1560307fdc
Deepthi1997-QISCET <41726078+Deepthi1997-QISCET@users.noreply.github.com>
2018-07-31 10:11:51 +0530

    Update Power.java

--- Power.java
+++ Power.java
@@ -1,14 +1,18 @@
 import java.util.*;
 import java.lang.*;
 import java.io.*;
+import java.util.Scanner;
 
 /* Name of the class has to be "Main" only if the class is public. */
-class Power.java
+class Power
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-		int a=3,b=2;
-		double result=Math.pow(a,b);
-	System.out.println("Answer is:" +result);
+		int a,b;
+		Scanner s=new Scanner(System.in);
+		a=s.nextInt();
+		b=s.nextInt();
+		int res=(int)Math.pow(a,b);
+	        System.out.println(res);
 	}
 }

ab57deb75f0b1805bcedb70fee0fe75c191a5a69
Deepthi1997-QISCET <41726078+Deepthi1997-QISCET@users.noreply.github.com>
2018-07-31 14:39:12 +0530

    Update Prime.java

--- Prime.java
+++ Prime.java
@@ -1,18 +1,33 @@
+/* package whatever; // don't place package name! */
+
 import java.util.*;
 import java.lang.*;
 import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
 class Prime
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-	int i=11;
-	if(i%1==0&&i%i==0)
-	{
-	  System.out.println("yes");
-	}
-	else
-	{
-		System.out.println("no");
-	}
+	Scanner sc=new Scanner(System.in);
+	int n=sc.nextInt();
+	int count=0;
+		for(int i=2;i<n;i++)
+		{
+			if(n%i==0)
+			{
+				count++;
+			}
+			
+		}
+		if(count==0)
+		{
+		System.out.println("Yes");
+		}
+		else
+		{
+		System.out.println("no");	
+		}
+
 	}
 }

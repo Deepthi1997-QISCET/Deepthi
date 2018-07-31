2885d88707870f19958514dff2d5f80de2ffed53
Deepthi1997-QISCET <41726078+Deepthi1997-QISCET@users.noreply.github.com>
2018-07-31 10:37:56 +0530

    Update Palindrome.java

--- Palindrome.java
+++ Palindrome.java
@@ -3,25 +3,26 @@ import java.lang.*;
 import java.io.*;
 class Palindrome
 {
-	public static void main (String[] args) throws java.lang.Exception
+	public static void main(String[] args)throws java.lang.Exception
 	{
-          int r,sum=0,temp;    
-          int n=121;  
-          temp=n;    
-          while(n>0)
-          {    
-           r=n%10;   
-           sum=(sum*10)+r;    
-           n=n/10;    
-           }    
-         if(temp==sum)
-	 {
-         System.out.println("yes"); 
-	 }
-         else
-	 {
-         System.out.println("no");
-	 }
+		int r,sum=0,temp,n;
+		Scanner s=new Scanner(System.in);
+		n=s.nextInt();
+		temp=n;
+		while(n>0)
+		{
+			r=n%10;
+			sum=(sum*10)+r;
+			n=n/10;
+		}
+		if(temp==sum)
+		{
+			System.out.println("yes");
+		}
+		else
+		{
+			System.out.println("no");
+		}
 	}
-	
 }
+			

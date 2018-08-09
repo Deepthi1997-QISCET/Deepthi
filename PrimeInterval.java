c979d64be32dc23f98e51b292a02608cba8c1cd3
Deepthi1997-QISCET <41726078+Deepthi1997-QISCET@users.noreply.github.com>
2018-08-09 12:04:24 +0530

    Update PrimeInterval.java

--- PrimeInterval.java
+++ PrimeInterval.java
@@ -1,34 +1,33 @@
 import java.util.*;
 import java.lang.*;
 import java.io.*;
+import java.util.Scanner;
 
 /* Name of the class has to be "Main" only if the class is public. */
 class PrimeInterval
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-	       int i =0;
-       int num =0;
-       //Empty String
-       String  primeNumbers = "";
-
-       for (i = 1; i <= 10; i++)         
-       { 		  	  
-          int counter=0; 	  
-          for(num =i; num>=1; num--)
-	  {
-             if(i%num==0)
-	     {
- 		counter = counter + 1;
-	     }
-	  }
-	  if (counter ==2)
-	  {
-	     //Appended the Prime number to the String
-	     primeNumbers = primeNumbers + i + " ";
-	  }	
-       }	
-       System.out.println("Prime numbers from 1 to 10 are :");
-       System.out.println(primeNumbers);
+		int low,high;
+		Scanner s=new Scanner(System.in);
+		low=s.nextInt();
+		high=s.nextInt();
+		while(low<high)
+		{
+			boolean b=false;
+			for(int i=2;i<=low/2;i++)
+			{
+				if(low%i==0)
+				{
+					b=true;
+					break;
+				}
+			}
+			if(!b)
+			{
+				System.out.println(low + " ");
+			}
+			low++;
+		}
 	}
 }

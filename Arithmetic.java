2cfe23b2c4410caf5246f9ade13353b8cf32e087
Deepthi1997-QISCET <41726078+Deepthi1997-QISCET@users.noreply.github.com>
2018-08-09 07:26:44 +0530

    Update Arithmetic.java

--- Arithmetic.java
+++ Arithmetic.java
@@ -5,15 +5,15 @@ import java.lang.*;
 import java.io.*;
 
 /* Name of the class has to be "Main" only if the class is public. */
-class Ideone
+class Arthimetic
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
 	int n,a,d;
 	Scanner sc = new Scanner(System.in);
 	n=sc.nextInt();
-            a=sc.nextInt();
-            d=sc.nextInt();
-            System.out.print(n*(2*a+(n-1)*d)/2);
+        a=sc.nextInt();
+        d=sc.nextInt();
+        System.out.print(n*(2*a+(n-1)*d)/2);
 	}
 }

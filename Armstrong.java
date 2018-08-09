f22eaa7a8ac6b6f5461cd7bfa913b6cfa699f133
Deepthi1997-QISCET <41726078+Deepthi1997-QISCET@users.noreply.github.com>
2018-08-09 11:42:51 +0530

    Update Armstrong.java

--- Armstrong.java
+++ Armstrong.java
@@ -8,23 +8,23 @@ class Armstrong
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-	 int n,c=0,a,temp;
-    Scanner s=new Scanner(System.in);
-    n=s.nextInt();
-    temp=n;
-    while(n!=0)  
-    {  
-    a=n%10;  
-    n=n/10;  
-    c=c+(a*a*a);  
-    }  
-    if(temp==c)  
-    {
-    System.out.println("yes");
-    }
-    else
-    {
-        System.out.println("no");
-    }
-   }
+		int n,c=0,a,temp;
+		Scanner s=new Scanner(System.in);
+		n=s.nextInt();
+		temp=n;
+		while(n!=0)
+		{
+			a=n%10;
+			n=n/10;
+			c=c+(a*a*a);
+		}
+		if(temp==c)
+		{
+			System.out.println("yes");
+		}
+		else
+		{
+			System.out.println("no");
+		}
+	}
 }

a168b9813634d29aba4ddcb8c05d20b1bdd26cc2
Deepthi1997-QISCET <41726078+Deepthi1997-QISCET@users.noreply.github.com>
2018-08-13 12:43:02 +0530

    Update NWord.java

--- NWord.java
+++ NWord.java
@@ -11,25 +11,25 @@ class Ideone
 		int n=sc.nextInt();
 		switch (n)
 		{
-			case 1: System.out.print("One ");
+			case 1: System.out.print("One");
 			break;
-			case 2: System.out.print("Two "); 
+			case 2: System.out.print("Two"); 
 			break;
-			case 3: System.out.print("Three "); 
+			case 3: System.out.print("Three"); 
 			break;
-			case 4: System.out.print("Four "); 
+			case 4: System.out.print("Four"); 
 			break;
-			case 5: System.out.print("Five ");
+			case 5: System.out.print("Five");
 			break;
-			case 6: System.out.print("Six "); 
+			case 6: System.out.print("Six"); 
 			break;
-			case 7: System.out.print("Seven "); 
+			case 7: System.out.print("Seven"); 
 			break;
-			case 8: System.out.print("Eight ");
+			case 8: System.out.print("Eight");
 			break;	
-			case 9: System.out.print("Nine ");
+			case 9: System.out.print("Nine");
 			break;
-			case 10: System.out.print("Ten "); 
+			case 10: System.out.print("Ten"); 
 			break;
 			default: System.out.print(""); 
 			break;

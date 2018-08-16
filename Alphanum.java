33059d547c40afaa8996f3fbcc0a1a2dbbe2a71f
Deepthi1997-QISCET <41726078+Deepthi1997-QISCET@users.noreply.github.com>
2018-08-16 10:46:21 +0530

    Create Alphanum.java

---
+++ Alphanum.java
@@ -0,0 +1,39 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int count=0;
+		int count1=0;
+		int count2=0;
+		String s=sc.nextLine();
+		for(int i=0;i<s.length();i++)
+		{
+			if(Character.isDigit(s.charAt(i)))
+			{
+				count++;
+			}
+			else if(Character.isAlphabetic(s.charAt(i)))
+			{
+				count1++;
+			}
+			else
+			{
+				count2++;
+			}
+		}
+			if(count>0 && count1>0)
+			{
+				System.out.println("Yes");
+			}
+			else
+			{
+				System.out.println("No");
+			}
+	}
+}

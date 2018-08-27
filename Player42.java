91e0723ae063a2762851d5f7ff6e02344a9d3e45
Deepthi1997-QISCET <41726078+Deepthi1997-QISCET@users.noreply.github.com>
2018-08-27 12:44:10 +0530

    Create Player42.java

---
+++ Player42.java
@@ -0,0 +1,37 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Player42
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int k=sc.nextInt();
+		int a[]=new int[n];
+		int i,count=0;	
+		for(i=0;i<n-1;i++)
+		{
+			a[i]=sc.nextInt();
+		}
+		for(i=0;i<n-1;i++)
+		{
+			if(a[i]==k)
+			{
+				count=1;
+			}
+		}
+		if(count==1)
+		{
+			System.out.print("yes");
+		}
+		else
+		{
+			System.out.print("no");
+		}
+	}
+}

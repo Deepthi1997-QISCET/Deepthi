ae1ea04f84e2e1374ee1fb28e1762b5055f2b9bc
Deepthi1997-QISCET <41726078+Deepthi1997-QISCET@users.noreply.github.com>
2018-09-07 14:34:35 +0530

    Create Player97.java

---
+++ Player97.java
@@ -0,0 +1,25 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		int i,sum=0;
+		for(i=a;i<=b;i++)
+		{
+			if(i%2!=0)
+			{
+				sum=sum+i;
+			}
+		}
+		System.out.println(sum);
+	}
+}

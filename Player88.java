72fc3831592127dccc0b5c747adc6ec69f869b77
Deepthi1997-QISCET <41726078+Deepthi1997-QISCET@users.noreply.github.com>
2018-09-09 22:39:12 +0530

    Create Player88.java

---
+++ Player88.java
@@ -0,0 +1,19 @@
+/* package whatever; // don't place package name! */
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int n,k;
+		float m;
+		Scanner sc=new Scanner(System.in);
+		n=sc.nextInt();
+		k=sc.nextInt();
+		m=n>>k;
+		String s = String.format ("%.2f", m);
+		System.out.print(s);
+	}
+}

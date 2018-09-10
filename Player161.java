0973023d37c5a27fd2b8cb63383128be3f565642
Deepthi1997-QISCET <41726078+Deepthi1997-QISCET@users.noreply.github.com>
2018-09-10 14:42:20 +0530

    Create Player161.java

---
+++ Player161.java
@@ -0,0 +1,40 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		String s[]=new String[n];
+		int i,j,count=0;
+		for(i=0;i<n;i++)
+		{
+			s[i]=sc.nextLine();
+		}
+		for(i=0;i<n;i++)
+		{
+			for(j=i;j<s[i].length();j++)
+			{
+				if(s[i].charAt(j)=='a'||s[i].charAt(j)=='e'||s[i].charAt(j)=='i'||s[i].charAt(j)=='o'||s[i].charAt(j)=='u')
+				{
+					count++;
+				}
+				else
+				{
+					continue;
+				}
+			}
+		}
+		if(count>0)
+		{
+			System.out.print("yes");
+		}
+		else
+		{
+			System.out.print("no");
+		}
+	}
+}

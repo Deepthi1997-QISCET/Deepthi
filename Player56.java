a320f43bf61ec8856c0e389441d606eafddebf75
Deepthi1997-QISCET <41726078+Deepthi1997-QISCET@users.noreply.github.com>
2018-09-10 15:50:28 +0530

    Create Player56.java

---
+++ Player56.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		String s=sc.next();
+		char k=sc.next().charAt(0);
+		int i,count=0;
+		for(i=0;i<s.length();i++)
+		{
+			if(s.charAt(i)==k)
+			{
+			System.out.print(i+1);
+			break;
+			}
+		}
+	}
+}

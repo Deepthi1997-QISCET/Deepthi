985c12ebc89e92d7e7334ccde0bc3da9ee5393a2
Deepthi1997-QISCET <41726078+Deepthi1997-QISCET@users.noreply.github.com>
2018-09-18 14:43:21 +0530

    Create Player114.java

---
+++ Player114.java
@@ -0,0 +1,21 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		long a=sc.nextInt();
+		long b=sc.nextInt();
+		long c=sc.nextInt();
+		long d=(long)Math.pow(a,b);
+		long e=(long)d%c;
+		System.out.println(e);
+	}
+}

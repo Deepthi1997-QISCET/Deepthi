cf5ef7857dbc64ae64b4d27a1df8c9f2485d59aa
Deepthi1997-QISCET <41726078+Deepthi1997-QISCET@users.noreply.github.com>
2018-09-20 15:12:57 +0530

    Create Player194.java

---
+++ Player194.java
@@ -0,0 +1,33 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		String s1=sc.next();
+		String s2=sc.next();
+		if(((s1.equals("P"))&&(s2.equals("S")))||((s1.equals("S"))&&(s2.equals("P"))))
+		{
+			System.out.print("S");
+		}
+		else if(((s1.equals("P"))&&(s2.equals("R")))||((s1.equals("R"))&&(s2.equals("P"))))
+		{
+			System.out.print("P");
+		}
+		else if(s1.equals(s2))
+		{
+			System.out.print("D");
+		}
+		else
+		{
+			System.out.print("R");
+		}
+	}
+}
